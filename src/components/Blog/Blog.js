fd51d7686d33af2cb2f0d2ceccfc61b3398c229a
Rakibul2580 <rh565153@gmail.com>
2022-10-11 19:57:46 +0300

    update the blog text

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,31 +2,45 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div className="h-screen bg-white">
-      <div className="">
-        <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
-          <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-            Focus me to see content
-          </div>
-          <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-          </div>
+    <div className="">
+      <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
+        <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+          1. What is the purpose of React Router ?
         </div>
-        <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
-          <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-            Focus me to see content
-          </div>
-          <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-          </div>
+        <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+          <p>
+            ReactJS Router is mainly used for developing Single Page Web
+            Applications. React Router is used to define multiple routes in the
+            application.
+          </p>
         </div>
-        <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
-          <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-            Focus me to see content
-          </div>
-          <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-          </div>
+      </div>
+      <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
+        <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+          2. How does Context API work ?
+        </div>
+        <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+          <p>
+            The React Context API is a way for a React app to effectively
+            produce global variables that can be passed around. This is the
+            alternative to "prop drilling" or moving props from grandparent to
+            child to parent, and so on. Context is also touted as an easier,
+            lighter approach to state management using Redux.
+          </p>
+        </div>
+      </div>
+      <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
+        <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+          3. What is useRef?
+        </div>
+        <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+          <p>
+            useRef is like a “box” that can hold a mutable value in its .current
+            property. The useRef Hook allows you to persist values between
+            renders. It can be used to store a mutable value that does not cause
+            a re-render when updated. It can be used to access a DOM element
+            directly.
+          </p>
         </div>
       </div>
     </div>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -16,31 +16,6 @@ const Statistics = () => {
   const data = useContext(ItemsContext);
   return (
     <div className="h-screen bg-white flex items-center justify-center w-full">
-      {/* <ResponsiveContainer width={300} height={350}>
-        <LineChart
-          width={500}
-          height={200}
-          data={data}
-          syncId="anyId"
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Line
-            type="monotone"
-            dataKey="total"
-            stroke="#8884d8"
-            fill="#8884d8"
-          />
-        </LineChart>
-      </ResponsiveContainer> */}
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart
           width={500}
