a2206aef89166aebb454c397680faef2bde401c5
Rakibul2580 <rh565153@gmail.com>
2022-10-11 21:25:31 +0300

    updata blog and redme

--- README.md
+++ README.md
@@ -16,8 +16,8 @@
 
 ### Use SweetAlert2 :
 
-#### SweetAlert2 is vary easy and A BEAUTIFUL, RESPONSIVE, CUSTOMIZABLE, ACCESSIBLE (WAI-ARIA) REPLACEMENT FOR JAVASCRIPT'S POPUP BOXES : https://sweetalert2.github.io/
+#### SweetAlert2 is vary easy and A beautiful, responsive, customizable, accessible (wai-aria)replacement for javascript's popup boxes : https://sweetalert2.github.io/
 
 ### Use LineChart :
 
-#### SweetAlert2 is vary easy and The main purpose of this library is to help you to write charts in React applications without any pain. : https://recharts.org/en-US/examples
+#### LineChart is vary easy and The main purpose of this library is to help you to write charts in React applications without any pain. : https://recharts.org/en-US/examples
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,44 +3,46 @@ import React from "react";
 const Blog = () => {
   return (
     <div className="mt-16 h-full md:h-screen bg-white">
-      <div tabIndex={0} className="collapse group mt-16 rounded-md mx-10">
-        <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-          1. What is the purpose of React Router ?
+      <div>
+        <div tabIndex={0} className="collapse group mt-16 rounded-md mx-10">
+          <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+            1. What is the purpose of React Router ?
+          </div>
+          <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+            <p>
+              ReactJS Router is mainly used for developing Single Page Web
+              Applications. React Router is used to define multiple routes in
+              the application.
+            </p>
+          </div>
         </div>
-        <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-          <p>
-            ReactJS Router is mainly used for developing Single Page Web
-            Applications. React Router is used to define multiple routes in the
-            application.
-          </p>
+        <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
+          <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+            2. How does Context API work ?
+          </div>
+          <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+            <p>
+              The React Context API is a way for a React app to effectively
+              produce global variables that can be passed around. This is the
+              alternative to "prop drilling" or moving props from grandparent to
+              child to parent, and so on. Context is also touted as an easier,
+              lighter approach to state management using Redux.
+            </p>
+          </div>
         </div>
-      </div>
-      <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
-        <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-          2. How does Context API work ?
-        </div>
-        <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-          <p>
-            The React Context API is a way for a React app to effectively
-            produce global variables that can be passed around. This is the
-            alternative to "prop drilling" or moving props from grandparent to
-            child to parent, and so on. Context is also touted as an easier,
-            lighter approach to state management using Redux.
-          </p>
-        </div>
-      </div>
-      <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
-        <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-          3. What is useRef?
-        </div>
-        <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
-          <p>
-            useRef is like a “box” that can hold a mutable value in its .current
-            property. The useRef Hook allows you to persist values between
-            renders. It can be used to store a mutable value that does not cause
-            a re-render when updated. It can be used to access a DOM element
-            directly.
-          </p>
+        <div tabIndex={0} className="collapse group mt-10 rounded-md mx-10">
+          <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+            3. What is useRef?
+          </div>
+          <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+            <p>
+              useRef is like a “box” that can hold a mutable value in its
+              .current property. The useRef Hook allows you to persist values
+              between renders. It can be used to store a mutable value that does
+              not cause a re-render when updated. It can be used to access a DOM
+              element directly.
+            </p>
+          </div>
         </div>
       </div>
     </div>
