cf1c5904ee858202a451c7e1ebff0b393dfb910a
Rakibul2580 <rh565153@gmail.com>
2022-10-11 12:48:12 +0300

    quiz complet

--- src/components/Quis/Prinquis.js
+++ src/components/Quis/Prinquis.js
@@ -1,32 +1,26 @@
 import React from "react";
+import Swal from "sweetalert2";
 
-const Prinquis = ({ item }) => {
-  console.log(item);
+const Prinquis = ({ item, handleAns }) => {
   const { question, options, correctAnswer } = item;
-  const x = (a, b) => {
-    if (a == b) {
-      alert("Right");
-    } else {
-      alert("Rog");
-    }
-  };
+
   return (
     <div>
       <div className="border-4 border-sky-300 rounded-md shadow-2xl m-10 p-5 bg-white">
-        <h1 className="text-2xl font-semibold text-black">Quiz : {question}</h1>
+        <h1 className="text-2xl font-semibold text-black">
+          Quiz : {question.replace("<p>", " ").replace("</p>", " ")}
+        </h1>
         <div className="grid grid-cols-2 gap-4 mt-3">
           {options.map((option) => (
-            <div
-              onClick={() => x(correctAnswer, option)}
-              className="flex items-center p-3 rounded-md bg-sky-200 shadow-xl text-black mt-3"
-            >
+            <div className="flex items-center p-3 rounded-md bg-sky-200 shadow-xl text-black font-medium mt-3">
               <input
+                onClick={() => handleAns(correctAnswer, option)}
                 type="radio"
                 name="radio"
                 className="radio radio-accent"
                 checked
               />
-              <p className="ml-2 text-black font-medium">{option}</p>
+              <p className="ml-2">{option}</p>
             </div>
           ))}
         </div>
--- src/components/Quis/Quis.js
+++ src/components/Quis/Quis.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Prinquis from "./Prinquis";
+import Swal from "sweetalert2";
 
 const Quis = () => {
   const datas = useLoaderData();
+  const [truecount, setTruecount] = useState(0);
+  const [falsecount, setFalsecount] = useState(0);
+  const handleAns = (correctAnswer, option) => {
+    if (correctAnswer == option) {
+      Swal.fire({
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setTruecount((count) => count + 1);
+    } else {
+      Swal.fire({
+        icon: "warning",
+        title: "Are you sure?",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setFalsecount((count) => count + 1);
+    }
+  };
   return (
-    <div>
-      {datas.data.questions.map((item) => (
-        <Prinquis key={item.id} item={item}></Prinquis>
-      ))}
+    <div className="flex">
+      <div className="w-9/12">
+        {datas.data.questions.map((item) => (
+          <Prinquis key={item.id} item={item} handleAns={handleAns}></Prinquis>
+        ))}
+      </div>
+      <div className="w-3/12">
+        <div className=" bg-sky-400 py-4 px-1 sticky top-0 rounded-md mt-10 mr-5">
+          <h1 className="text-white text-3xl font-bold">Marks</h1>
+          <h4 className="text-white text-lg font-medium mt-2">
+            Total Quiz: {datas.data.questions.length}
+          </h4>
+          <h4 className="text-white text-lg font-medium my-2">
+            True Ans: {truecount}
+          </h4>
+          <h4 className="text-white text-lg font-medium ">
+            False Ans: {falsecount}
+          </h4>
+        </div>
+      </div>
     </div>
   );
 };
