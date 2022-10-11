a38515ee4c72ee939fc6a253b717cf2b777c1555
Rakibul2580 <rh565153@gmail.com>
2022-10-11 19:09:47 +0300

    chang banner text

--- src/components/Home/Baner.js
+++ src/components/Home/Baner.js
@@ -7,11 +7,11 @@ const Banner = () => {
       <div>
         <div className="bgimg pb-72 bg-gradient-to-r from-violet-500 to-fuchsia-500">
           <h1 className="text-6xl font-bold text-white pt-60">
-            Material Kit 2
+            Welcome check your coding ability
           </h1>
           <p className="text-2xl font-medium text-white mt-7">
-            Free & Open Source Web UI Kit built over Bootstrap 5. Join over 1.6
-            million developers around the world.
+            Hard work is the key to success. Work hard today rest tomorrow.
+            Remember that time waits for no one.
           </p>
         </div>
       </div>
